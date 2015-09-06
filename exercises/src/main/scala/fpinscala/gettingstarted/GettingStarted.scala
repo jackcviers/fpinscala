03501ac92d3f19f4d913425e45e2bae9da8ebfc6
Jack Viers <jackcviers@gmail.com>
2015-09-06 11:42:03 -0500

    Ex 2.5

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -159,21 +159,23 @@ object PolymorphicFunctions {
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
 
-  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+  // Exercise 3: Implement `partial1`.
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
     (b: B) => f(a, b)
 
   // Exercise 3: Implement `curry`.
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a, b)
 
   // NB: The `Function2` trait has a `curried` method already
 
-  // Exercise 4: Implement `uncurry`
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+  // Exercise 5: Implement `uncurry`
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b:B) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -187,6 +189,6 @@ object PolymorphicFunctions {
 
   // Exercise 5: Implement `compose`
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    (a:A) => f(g(a))
 }
--- exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedSpec.scala
+++ exercises/src/test/scala/fpinscala/gettingstarted/GettingStartedSpec.scala
@@ -3,21 +3,35 @@ package fpinscala.gettingstarted
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 import scala.collection.JavaConversions._
+import PolymorphicFunctions._
 
 class GettingStartedSpec extends Specification with ScalaCheck {
 
-
-  "PolymorphicFunctions.sortInfo[Int]" >> {
-    def gt(i: Char, j: Char) = i >= j
-    prop { (as: Array[Char]) =>
-      PolymorphicFunctions.isSorted(as.sorted, gt)
+  "PolymorphicFunctions" >> {
+    "isSorted[A]" >> {
+      def gt(i: Char, j: Char) = i >= j
+      def gtS(i: Short, j: Short) = i >= j
+      prop { (as: Array[Char]) =>
+        isSorted(as.sorted, gt)
+      } && prop{as: Array[Short] =>
+        isSorted(as.sorted, gtS)
+      }
+    }
+    "curry[A, B, C]" >> {
+      prop{ (f: (Int, Int) => Int, i: Int)  =>
+        curry(f)(i)(i) must_== f(i, i)
+      }
+    }
+    "uncurry[A, B, C]" >> {
+      prop{ (f: (Int) => (Int) => Int, i: Int) =>
+       uncurry(f)(i, i) must_== f(i)(i)
+      }
+    }
+    "compose[A,B,C]" >> {
+      prop{(f: Char => String, g: Int => Char, i:Int) =>
+        compose(f,g)(i) must_== f(g(i))
+      }
     }
   }
+
 }
-
-// import org.specs2.mutable.Specification
-// import org.specs2.ScalaCheck
-
-// class GettingStartedSpec extends Specification with ScalaCheck
-// // class GettingStartedSpec extends Specification with ScalaCheck
-// // class GettingStartedSpec extends Specification with ScalaCheck
