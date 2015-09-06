84f84fbb4b2c1b3bcbd758377a8184f76a178309
Jack Viers <jackcviers@gmail.com>
2015-09-06 13:31:21 -0500

    Exercies 3.4

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -7,56 +7,60 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 object List { // `List` companion object
   def sum(ints: List[Int]): Int = ints match { // Pattern matching example
     case Nil => 0
-    case Cons(x,xs) => x + sum(xs)
-  } 
-  
+    case Cons(x, xs) => x + sum(xs)
+  }
+
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs) => x * product(xs)
   }
-  
+
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
-  
+
   val example = Cons(1, Cons(2, Cons(3, Nil))) // Creating lists
-  val example2 = List(1,2,3)
+  val example2 = List(1, 2, 3)
   val total = sum(example)
 
-  val x = List(1,2,3,4,5) match {
+  val x = List(1, 2, 3, 4, 5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42 
+    case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
     case Cons(h, t) => h + sum(t)
-    case _ => 101 
+    case _ => 101
   }
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+      case Cons(h, t) => Cons(h, append(t, a2))
     }
 
-  def foldRight[A,B](l: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+  def foldRight[A, B](l: List[A], z: B)(f: (A, B) => B): B = // Utility functions
     l match {
       case Nil => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
-  
-  def sum2(l: List[Int]) = 
-    foldRight(l, 0.0)(_ + _)
-  
-  def product2(l: List[Double]) = 
-    foldRight(l, 1.0)(_ * _)
 
+  def sum2(l: List[Int]) =
+    foldRight(l, 0.0)(_ + _)
+
+  def product2(l: List[Double]) =
+    foldRight(l, 1.0)(_ * _)
 
   def tail[A](l: List[A]): List[A] = l match {
     case Cons(x, xs) => xs
     case _ => Nil
   }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+    case Cons(x, xs) if n < 1 => l
+    case Cons(x, xs) if n == 1 => xs
+    case Nil => Nil
+    case Cons(_, xs) => drop(xs, n - 1)
+  }
 
   def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = sys.error("todo")
 
@@ -64,9 +68,15 @@ object List { // `List` companion object
 
   def init[A](l: List[A]): List[A] = sys.error("todo")
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def length[A](l: List[A]): Int = {
+    def loop(as: List[A], count: Int = 0): Int = as match {
+      case Cons(_, xs) => loop(xs, count + 1)
+      case Nil => count
+    }
+    loop(l)
+  }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def map[A, B](l: List[A])(f: A => B): List[B] = sys.error("todo")
 }
