f000abb00ff9895f726f7f87310540c92855794b
Jack Viers <jackcviers@gmail.com>
2015-09-06 12:08:17 -0500

    Completes ex 3.1

--- exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -1,7 +1,15 @@
 package fpinscala.datastructures
 
+import List._
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
 
-class ListSpec extends Specification with ScalaCheck
+class ListSpec extends Specification with ScalaCheck {
+  "List" >> {
+    "exercise1" >> {
+      x must_== 3
+    }
+
+  }
+}
