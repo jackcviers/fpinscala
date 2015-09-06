292b4d5249436599ead22505f08e413e6fecab26
Jack Viers <jackcviers@gmail.com>
2015-09-06 12:58:50 -0500

    Completes ex 3.2

--- exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -1,6 +1,10 @@
 package fpinscala.datastructures
 
 import List._
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import org.scalacheck.Gen._
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
@@ -10,6 +14,22 @@ class ListSpec extends Specification with ScalaCheck {
     "exercise1" >> {
       x must_== 3
     }
+    "tail" >> {
 
+
+      prop {a: List[Char] =>
+        tail(a) == (a match{
+          case Cons(x, xs) => xs
+          case Nil => Nil
+        })
+      }
+    }
   }
+  def genNil = const(Nil)
+  def genList = for{
+    i <- arbitrary[Char]
+    b <- arbitrary[Char]
+  } yield Cons(i, List(b))
+
+  implicit def abList: Arbitrary[List[Char]] = Arbitrary(Gen.oneOf(genNil, genList))
 }
