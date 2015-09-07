83eeb04632e7071d674ce520676eae26222f34b5
Jack Viers <jackcviers@gmail.com>
2015-09-07 17:39:01 -0500

    Completes Ex 4.3

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,15 +4,47 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  /*
+  Of course, we can also implement `flatMap` with explicit pattern matching.
+  */
+  def flatMap_1[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    this map (Some(_)) getOrElse ob
+
+  /*
+  Again, we can implement this with explicit pattern matching.
+  */
+  def orElse_1[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if f(a) => this
+    case _ => None
+  }
+  /*
+  This can also be defined in terms of `flatMap`.
+  */
+  def filter_1(f: A => Boolean): Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -23,6 +55,7 @@ object Option {
     try {
       val x = 42 + 5
       x + y
+<<<<<<< HEAD
     }
     catch { case e: Exception => 43 } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
   }
@@ -33,6 +66,9 @@ object Option {
       x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
     }
     catch { case e: Exception => 43 }
+=======
+    } catch { case e: Exception => 43 }
+>>>>>>> Completes Ex 4.3
   }
 
   def mean(xs: Seq[Double]): Option[Double] =
@@ -40,7 +76,7 @@ object Option {
     else Some(xs.sum / xs.length)
 
   import java.util.regex._
-  
+
   def pattern(s: String): Option[Pattern] =
     try {
       Some(Pattern.compile(s))
@@ -48,15 +84,15 @@ object Option {
       case e: PatternSyntaxException => None
     }
 
-  def mkMatcher(pat: String): Option[String => Boolean] = 
+  def mkMatcher(pat: String): Option[String => Boolean] =
     pattern(pat) map (p => (s: String) => p.matcher(s).matches) // The details of this API don't matter too much, but `p.matcher(s).matches` will check if the string `s` matches the pattern `p`.
 
-  def mkMatcher_1(pat: String): Option[String => Boolean] = 
+  def mkMatcher_1(pat: String): Option[String => Boolean] =
     for {
       p <- pattern(pat)
     } yield ((s: String) => p.matcher(s).matches)
-  
-  def doesMatch(pat: String, s: String): Option[Boolean] = 
+
+  def doesMatch(pat: String, s: String): Option[Boolean] =
     for {
       p <- mkMatcher_1(pat)
     } yield p(s)
@@ -68,14 +104,15 @@ object Option {
     } yield f(s) && g(s)
 
   def bothMatch_1(pat: String, pat2: String, s: String): Option[Boolean] =
-    mkMatcher(pat) flatMap (f => 
-    mkMatcher(pat2) map     (g => 
-    f(s) && g(s)))
-  def variance(xs: Seq[Double]): Option[Double] = if(xs == Seq.empty) None else mean(xs) flatMap { m => mean(xs map { x => math.pow(x - m, 2) }) }
+    mkMatcher(pat) flatMap (f =>
+      mkMatcher(pat2) map (g =>
+        f(s) && g(s)))
+  def variance(xs: Seq[Double]): Option[Double] = if (xs == Seq.empty) None else mean(xs) flatMap { m => mean(xs map { x => math.pow(x - m, 2) }) }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a flatMap { c => b map { d => f(c, d) } }
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+
 }
--- exercises/src/test/scala/fpinscala/errorhandling/OptionSpec.scala
+++ exercises/src/test/scala/fpinscala/errorhandling/OptionSpec.scala
@@ -1,6 +1,8 @@
 package fpinscala.errorhandling
 
 import Option._
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen
 
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
@@ -13,6 +15,24 @@ class OptionSpec extends Specification with ScalaCheck {
       prop { xs: Seq[Double] =>
         (xs != Seq.empty && mean(xs).get != Double.NaN) ==> {
           variance(xs).getOrElse(0) must_== (mean(xs.map { x: Double => math.pow(x - mean(xs).get, 2) })).get
+        }
+      }
+    }
+    "map2" >> {
+      implicit def arbitraryOptionInt[T](implicit a: Arbitrary[T]): Arbitrary[Option[T]] = Arbitrary {
+        val genMaybe = for { e <- Arbitrary.arbitrary[T] } yield Some(e)
+        val genNone = for { e <- Arbitrary.arbitrary[T] } yield None
+        Gen.oneOf(genMaybe, genNone)
+      }
+      prop { (maybeA: Option[Int], maybeB: Option[String], f: (Int, String) => String) =>
+        (maybeA == None) ==> {
+          map2(maybeA, maybeB)(f) must_== (maybeA match {
+            case Some(c) => maybeB match {
+              case Some(d) => f(c, d)
+              case None => None
+            }
+            case None => None
+          })
         }
       }
     }
