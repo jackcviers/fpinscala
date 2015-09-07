e32b15a99f1a29a05145a56d0e77b8a87a42c16a
Jack Viers <jackcviers@gmail.com>
2015-09-07 12:04:03 -0500

    WIP Options

--- answers/src/main/scala/fpinscala/datastructures/List.scala
+++ answers/src/main/scala/fpinscala/datastructures/List.scala
@@ -318,10 +318,12 @@ object List { // `List` companion object. Contains functions for creating and wo
     case (Cons(h,t),Cons(h2,t2)) if h == h2 => startsWith(t, t2)
     case _ => false
   }
-  @annotation.tailrec
-  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match {
-    case Nil => sub == Nil
-    case _ if startsWith(sup, sub) => true
-    case Cons(h,t) => hasSubsequence(t, sub)
+  def hasSubsequence[A](l: List[A], sub: List[A]): Boolean = {
+    @annotation.tailrec
+    def go[A](l: List[A]): Boolean = l match {      case Nil => false
+      case Cons(h,t) if startsWith(l, sub) => true
+      case Cons(h,t) => go(t)  
+    }
+    go(l)
   }
 }
